be68084134978caad1fec83a1d4c6e12c5514bc0
Chris Ryan <c.ryan@c02yx3uvlvdr.springernature.com>
2019-10-24 12:10:31 +0100

    Removes old components

--- src/components/Briefing/Briefing.stories.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-
-import Briefing from "./index"
-
-storiesOf("Briefing", module).add("default", () => <Briefing />)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Export the components from here
-// export { default as Head } from "./components/Head/index"
+export { default as Head } from "./components/Head/index"
 export { default as Test } from "./components/Test/index"
-// export { default as Menu } from "./components/Menu/index"
-// export { default as MenuStyle } from "./components/Menu/style.scss"
+export { default as Menu } from "./components/Menu/index"
+export { default as Footer } from "./components/Footer/index"
